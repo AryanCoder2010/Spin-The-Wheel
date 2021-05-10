6939ce58f8ca050b45329745d622a2a7a26b535f
AryanCoder2010 <77927433+AryanCoder2010@users.noreply.github.com>
2021-05-10 16:00:08 +0530

    numbers changed
    numbers changed

--- spin.js
+++ spin.js
@@ -1,6 +1,6 @@
 function wheel_spin(){
-       var a = 63975;
-       var b = 64265;
+       var a = 63975234567898765434567890;
+       var b = 64265234567890987654323456;
        var m = Math.floor(Math.random()*(a-b))+b;
        document.getElementById("spin").style.transform="rotate("+m+"deg)";
 
