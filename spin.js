c51622b9b7e298fffade33b6c73e1b9cac83d508
Aryan Ayushman Sahoo <77927433+AryanCoder2010@users.noreply.github.com>
2022-09-26 11:12:07 +0530

    Update spin.js

--- spin.js
+++ spin.js
@@ -1,6 +1,6 @@
 function wheel_spin(){
-       var a = 63676076087572938695;
-       var b = 94794275024758356843;
+       var a = 1234567890;
+       var b = 1234567890;
        var m = Math.floor(Math.random()*(a-b))+b;
        document.getElementById("spin").style.transform="rotate("+m+"deg)";
 
